bf471045e6c7fd92ac5957448f26f6e70916167e
pmxhq <your@email.com>
2016-06-20 17:25:21 +0800

    Signed-off-by: pmxhq <your@email.com>

---
+++ 网通宝管理后台/files/银行对账管理/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k],l,_(m,n,o,p,q,r,s,_(),t,_(u,v,w,x,y,_(z,A,B,C),D,null,E,x,F,x,G,H,I,null,J,K,L,M,N,O,P,K),Q,_(),R,_(),S,_(T,[])),U,_(),V,_());}; 
+var b="url",c="银行对账管理.html",d="generationDate",e=new Date(1466414648912.29),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="rl_date",k="rl_day",l="page",m="packageId",n="2c41fe1c974f4b1ebf954ca77df49acc",o="type",p="Axure:Page",q="name",r="银行对账管理",s="notes",t="style",u="baseStyle",v="627587b6038d43cca051c114ac41ad32",w="pageAlignment",x="near",y="fill",z="fillType",A="solid",B="color",C=0xFFFFFFFF,D="image",E="imageHorizontalAlignment",F="imageVerticalAlignment",G="imageRepeat",H="auto",I="favicon",J="sketchFactor",K="0",L="colorStyle",M="appliedColor",N="fontName",O="应用字体",P="borderWidth",Q="adaptiveStyles",R="interactionMap",S="diagram",T="objects",U="masters",V="objectPaths";
+return _creator();
+})());
